2e374f5b1b17a5aeed3cbb6f8d857ea014c7820f
Sarah Patton <spatton@ferrum.edu>
2019-12-05 22:44:25 -0500

    Merge branch 'master' of https://github.com/pinkgokart/LoginDemo
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/logindemo/ProfileActivity.java
+++ app/src/main/java/com/example/logindemo/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.example.logindemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,6 +13,10 @@ public class ProfileActivity extends AppCompatActivity {
 
     private TextView SwipeLeft;
     private TextView DDLeft;
+    private Button Search;
+    private Button Pages;
+    private Button Home;
+    private Button Events;
 
 
     @Override
@@ -19,8 +26,60 @@ public class ProfileActivity extends AppCompatActivity {
 
         SwipeLeft =(TextView)findViewById(R.id.tvswipesleft);
         DDLeft =(TextView)findViewById(R.id.tvdiningdollarsleft);
+        Search =(Button)findViewById(R.id.btnsearch);
+        Pages =(Button)findViewById(R.id.btnpages);
+        Home =(Button)findViewById(R.id.btnhome);
+        Events =(Button)findViewById(R.id.btnevents);
+
+        Search.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openSearchActivity();
+            }
+        });
+
+        Pages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage1Activity();
+            }
+        });
+
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeActivity();
+            }
+        });
+
+        Events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEventsActivity();
+            }
+        });
 
         SwipeLeft.setText("Meal Swipes Left: " + 5);
         DDLeft.setText("Dining Dollars Left: $" + 100);
+    }
+
+    public void openSearchActivity() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPage1Activity() {
+        Intent intent = new Intent(this, Page1Activity.class);
+        startActivity(intent);
+    }
+
+    public void openHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void openEventsActivity() {
+        Intent intent = new Intent(this, EventsActivity.class);
+        startActivity(intent);
     }
 }
