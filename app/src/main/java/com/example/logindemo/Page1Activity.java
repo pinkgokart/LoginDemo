2dd99bed824fdf3b2f3aea7f8e9a0afcd361170b
kaldillard <mdillard@ferrum.edu>
2019-12-06 22:31:46 -0500

    ye

--- app/src/main/java/com/example/logindemo/Page1Activity.java
+++ app/src/main/java/com/example/logindemo/Page1Activity.java
@@ -3,12 +3,37 @@ package com.example.logindemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Page1Activity extends AppCompatActivity {
+
+    private Button Follow;
+    private Button Following;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
+
+        Follow = (Button)findViewById(R.id.btnFollow_0);
+        Following = (Button)findViewById(R.id.btnFollowing_0);
+
+        Follow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                 Follow.setText("following");
+                 Follow.setBackgroundColor(000000);
+
+            }
+        });
+
+        Following.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ();
+            }
+        });
     }
 }
