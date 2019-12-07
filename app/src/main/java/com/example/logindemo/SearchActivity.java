23eb242b31b458ecdd94d0d7539e33b1cff2174d
bestway1 <rphillips@ferrum.edu>
2019-12-07 16:18:06 -0500

    Search View

--- app/src/main/java/com/example/logindemo/SearchActivity.java
+++ app/src/main/java/com/example/logindemo/SearchActivity.java
@@ -3,7 +3,16 @@ package com.example.logindemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+<application ... >
+<activity android:name=".SearchableActivity" >
+<intent-filter>
+<action android:name="android.intent.action.SEARCH" />
+</intent-filter>
+<meta-data android:name="android.app.searchable"
+        android:resource="@xml/searchable"/>
+</activity>
+        ...
+</application>
 public class SearchActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +20,5 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
 }
